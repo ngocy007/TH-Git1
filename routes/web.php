7724771bb85fe4629bc37bc60f86030b548c7265
NHH-Viet <kingtung93@gmail.com>
2022-11-30 18:32:50 +0700

    dang nhap va email

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 
-    return redirect('/home');
+    return redirect('/dashboard');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {

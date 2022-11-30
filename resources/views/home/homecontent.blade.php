de5537695b96f996efb8480c1a0e29a79c8f1e14
Xuan Dat <xuandat282@gmail.com>
2022-11-30 21:16:19 +0700

    home content

---
+++ resources/views/home/homecontent.blade.php
@@ -0,0 +1,81 @@
+<link rel="stylesheet" href="{{asset('css/home.css')}}">
+<a href="#">
+    <span class="top-bg-op-box">
+        <img src="{{asset('asset/banner.webp')}}" alt="banner" style="width: 100%">
+    </span>
+</a>
+<div class="page-content rounded-2">
+    <div class="row">
+        <div class="col-8">
+            <section class="nh-section">
+                <div class="d-flex align-items-center mb-4">
+                    <h2 class="h4 mb-0">Biên tập viên đề cử</h2><a href="https://metruyencv.com/truyen?sort_by=new_chap_at&amp;props=1" class="link--see-more ml-auto text-primary"> Xem tất cả </a>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="media"><a href="https://metruyencv.com/truyen/dau-la-dai-luc-trung-sinh-duong-tam" class="nh-thumb nh-thumb--72 shadow mr-3"><img src="https://static.cdnno.com/poster/dau-la-dai-luc-trung-sinh-duong-tam/150.jpg?1621052117" alt="" width="72"></a>
+                            <div class="media-body">
+                                <h2 class="fz-body text-overflow-1-lines mb-2 "><a href="https://metruyencv.com/truyen/dau-la-dai-luc-trung-sinh-duong-tam" class="d-block">Đấu La Đại Lục Ⅴ Trùng Sinh Đường Tam</a></h2>
+                                <div class="text-secondary fz-14 text-overflow-2-lines"> Một đời Thần Vương trùng sinh tại Pháp Lam thế giới thần kỳ, hắn kinh ngạc phát hiện, nơi này hết thảy đều là như vậy kỳ diệu, hắn càng là vận khí tuyệt hảo tìm được thê tử chuyển thế thân. Thế nhưng là, nàng không có trí nhớ của kiếp trước, gặp nhau lần nữa đã là người qua đường.Một đời Thần Vương trùng sinh tại trong thế giới đối với nhân loại cũng không hữu hảo này, có thể hay không một lần nữa truy hồi thê tử. Thiên hình vạn trạng Yêu Thần Biến lại sẽ mang lại cho hắn như thế nào trùng sinh chi lộ? Đều ở một đời Thần Vương chí tình đuổi vợ hành trình, Đấu La đại lục bộ thứ năm, Trùng Sinh Đường Tam! </div>
+                                <div class="d-flex align-items-center mt-2 py-1">
+                                    <div class="d-flex align-items-center mr-auto text-secondary"><span class="truncate-140 text-left"><i class="nh-icon icon-user-edit mr-1"></i> Đường Gia Tam Thiếu </span></div><a href="https://metruyencv.com/truyen?genre=3"><span class="d-inline-block border border-primary small px-2 text-primary truncate-100">Huyền Huyễn</span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="media"><a href="https://metruyencv.com/truyen/van-tuong-chi-vuong" class="nh-thumb nh-thumb--72 shadow mr-3"><img src="https://static.cdnno.com/poster/van-tuong-chi-vuong/150.jpg?1631786834" alt="" width="72"></a>
+                            <div class="media-body">
+                                <h2 class="fz-body text-overflow-1-lines mb-2 "><a href="https://metruyencv.com/truyen/van-tuong-chi-vuong" class="d-block">Vạn Tướng Chi Vương</a></h2>
+                                <div class="text-secondary fz-14 text-overflow-2-lines"> Giữa thiên địa, có vạn tướng. Mà ta Lý Lạc, cuối cùng rồi sẽ trở thành Vạn Tướng Chi Vương này. </div>
+                                <div class="d-flex align-items-center mt-2 py-1">
+                                    <div class="d-flex align-items-center mr-auto text-secondary"><span class="truncate-140 text-left"><i class="nh-icon icon-user-edit mr-1"></i> Thiên Tàm Thổ Đậu </span></div><a href="https://metruyencv.com/truyen?genre=3"><span class="d-inline-block border border-primary small px-2 text-primary truncate-100">Huyền Huyễn</span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
+            </section>
+        </div>
+        <div class="col-4">
+            <section id="continue-reading" class="nh-section">
+                <div id="continue-reading-template">
+                    <div class="d-flex align-items-center mb-3">
+                        <h2 class="h4 mb-0">Đang đọc</h2><a href="https://metruyencv.com/tai-khoan/tu-truyen" class="link--see-more ml-auto text-primary"> Xem tất cả </a>
+                    </div>
+                    <ul class="list-unstyled m-0">
+                        <div data-v-f078dda2=""></div>
+                        <li id="item-reading-0" class="media align-items-center py-2 mb-1"><a href="https://metruyencv.com/truyen/dau-la-dai-luc-trung-sinh-duong-tam" class="nh-thumb nh-thumb--32 shadow mr-3"><img src="https://static.cdnno.com/poster/dau-la-dai-luc-trung-sinh-duong-tam/150.jpg?1621052117" width="32"></a>
+                            <div class="media-body">
+                                <h2 class="fz-body mb-1"><a href="https://metruyencv.com/truyen/dau-la-dai-luc-trung-sinh-duong-tam" class="text-overflow-1-lines"> Đấu La Đại Lục Ⅴ Trùng Sinh Đường Tam </a></h2>
+                                <div class="text-muted text-overflow-1-lines"> Đã đọc: 0/1168 </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</div>
+<section class="nh-section py-3">
+    <div class="d-flex align-items-center mb-3 ">
+        <h2 class="h4 mb-0"> Mới cập nhật </h2><a href="https://metruyencv.com/truyen" class="link--see-more ml-3 text-primary"> Xem tất cả </a>
+    </div>
+    <table class="table table-striped table-borderless table-hover border-top fz-14">
+        <tbody>
+            <tr>
+                <td class="align-middle text-tertiary"><span class="text-overflow-1-lines">Huyền Huyễn</span></td>
+                <td class="align-middle w-25">
+                    <h2 class="fz-body m-0 text-overflow-1-lines"><a href="https://metruyencv.com/truyen/vo-dich-tu-hien-te-to-su-gia-bat-dau">Vô Địch Từ Hiến Tế Tổ Sư Gia Bắt Đầu</a></h2>
+                </td>
+                <td class="align-middle w-25"><a href="https://metruyencv.com/truyen/vo-dich-tu-hien-te-to-su-gia-bat-dau/chuong-827" class="text-overflow-1-lines">Chương 827: Vì huynh đệ không tiếc mạng sống! Giúp ta chào hỏi tẩu tử</a></td>
+                <td class="align-middle"><span class="text-overflow-1-lines">Hạ Quân Cát</span></td>
+                <td class="align-middle text-tertiary"><span class="text-overflow-1-lines">SmileY</span></td>
+                <td class="align-middle text-tertiary text-right">3 phút trước</td>
+            </tr>
+        </tbody>
+    </table>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('master');
 });
+Route::get('/home', function () {
+    return view('home');
+});
 

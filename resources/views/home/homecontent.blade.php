c0cc47afa1f9414060f70ee4ff719fc9fb3b5989
Xuan Dat <xuandat282@gmail.com>
2022-12-01 11:01:54 +0700

    add home feature#1

--- resources/views/home/homecontent.blade.php
+++ resources/views/home/homecontent.blade.php
@@ -9,34 +9,24 @@
         <div class="col-8">
             <section class="nh-section">
                 <div class="d-flex align-items-center mb-4">
-                    <h2 class="h4 mb-0">Biên tập viên đề cử</h2><a href="https://metruyencv.com/truyen?sort_by=new_chap_at&amp;props=1" class="link--see-more ml-auto text-primary"> Xem tất cả </a>
+                    <h2 class="h4 mb-0">Biên tập viên đề cử</h2><a href="" class="link--see-more ml-auto text-primary"> Xem tất cả </a>
                 </div>
+                @foreach($truyens as $truyen)
                 <div class="row">
-                    <div class="col-6">
-                        <div class="media"><a href="https://metruyencv.com/truyen/dau-la-dai-luc-trung-sinh-duong-tam" class="nh-thumb nh-thumb--72 shadow mr-3"><img src="https://static.cdnno.com/poster/dau-la-dai-luc-trung-sinh-duong-tam/150.jpg?1621052117" alt="" width="72"></a>
+                    <div class="col-12">
+                        <div class="media"><a href="" class="nh-thumb nh-thumb--72 shadow mr-3"><img src="{{$truyen -> AnhDaiDien}}" alt="" width="72"></a>
                             <div class="media-body">
-                                <h2 class="fz-body text-overflow-1-lines mb-2 "><a href="https://metruyencv.com/truyen/dau-la-dai-luc-trung-sinh-duong-tam" class="d-block">Đấu La Đại Lục Ⅴ Trùng Sinh Đường Tam</a></h2>
-                                <div class="text-secondary fz-14 text-overflow-2-lines"> Một đời Thần Vương trùng sinh tại Pháp Lam thế giới thần kỳ, hắn kinh ngạc phát hiện, nơi này hết thảy đều là như vậy kỳ diệu, hắn càng là vận khí tuyệt hảo tìm được thê tử chuyển thế thân. Thế nhưng là, nàng không có trí nhớ của kiếp trước, gặp nhau lần nữa đã là người qua đường.Một đời Thần Vương trùng sinh tại trong thế giới đối với nhân loại cũng không hữu hảo này, có thể hay không một lần nữa truy hồi thê tử. Thiên hình vạn trạng Yêu Thần Biến lại sẽ mang lại cho hắn như thế nào trùng sinh chi lộ? Đều ở một đời Thần Vương chí tình đuổi vợ hành trình, Đấu La đại lục bộ thứ năm, Trùng Sinh Đường Tam! </div>
+                                <h2 class="fz-body text-overflow-1-lines mb-2 "><a href="" class="d-block">{{$truyen -> TenTruyen}}</a></h2>
+                                <div class="text-secondary fz-14 text-overflow-2-lines"> {{$truyen -> MoTa}} </div>
                                 <div class="d-flex align-items-center mt-2 py-1">
-                                    <div class="d-flex align-items-center mr-auto text-secondary"><span class="truncate-140 text-left"><i class="nh-icon icon-user-edit mr-1"></i> Đường Gia Tam Thiếu </span></div><a href="https://metruyencv.com/truyen?genre=3"><span class="d-inline-block border border-primary small px-2 text-primary truncate-100">Huyền Huyễn</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="media"><a href="https://metruyencv.com/truyen/van-tuong-chi-vuong" class="nh-thumb nh-thumb--72 shadow mr-3"><img src="https://static.cdnno.com/poster/van-tuong-chi-vuong/150.jpg?1631786834" alt="" width="72"></a>
-                            <div class="media-body">
-                                <h2 class="fz-body text-overflow-1-lines mb-2 "><a href="https://metruyencv.com/truyen/van-tuong-chi-vuong" class="d-block">Vạn Tướng Chi Vương</a></h2>
-                                <div class="text-secondary fz-14 text-overflow-2-lines"> Giữa thiên địa, có vạn tướng. Mà ta Lý Lạc, cuối cùng rồi sẽ trở thành Vạn Tướng Chi Vương này. </div>
-                                <div class="d-flex align-items-center mt-2 py-1">
-                                    <div class="d-flex align-items-center mr-auto text-secondary"><span class="truncate-140 text-left"><i class="nh-icon icon-user-edit mr-1"></i> Thiên Tàm Thổ Đậu </span></div><a href="https://metruyencv.com/truyen?genre=3"><span class="d-inline-block border border-primary small px-2 text-primary truncate-100">Huyền Huyễn</span></a>
+                                    <div class="d-flex align-items-center mr-auto text-secondary"><span class="truncate-140 text-left"><i class="nh-icon icon-user-edit mr-1"></i> {{$truyen -> TenTacGia}} </span></div><a href=""><span class="d-inline-block border border-primary small px-2 text-primary truncate-100">{{$truyen -> LuotXem}}</span></a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <hr>
-
+                @endforeach
             </section>
         </div>
         <div class="col-4">
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,42 +6,41 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Thể loại</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        @foreach($theloai as $th)
+                        <li><a class="dropdown-item" href="#">{{$th->TenLoai}}</a></li>
+                        @endforeach
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Bảng xếp hạng</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Đọc nhiều</a></li>
                     </ul>
                 </li>
             </ul>
-            <form class="nav-item form-group ml-auto w-25">
+            <form class="nav-item pt-2 form-group w-25">
                 <input class="form-control rounded-pill" type="text" placeholder="Tìm kiếm" aria-label="Search">
             </form>
+            <a class="nav-item" aria-current="page" href="#">Đăng truyện</a>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Đăng truyện</a>
-                </li>
                 @auth
                     @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                        <div class="shrink-0 mr-3">
-                            <img class="rounded-full object-cover" style="height: 40px;width: 40px" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                        </div>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img class="rounded-pill object-cover" style="height: 40px;width: 40px" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                {{ Auth::user()->name }}
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown-item">{{ Auth::user()->email }}</li>
+                                <li><a class="dropdown-item" href="{{ route('profile.show') }}">Hồ sơ</a></li>
+                                <li><form class="dropdown-item " method="post" action="{{ route('logout') }}" >
+                                        @csrf
+                                        <button class="border-0 bg-light rounded-2">Đăng xuất</button>
+                                    </form></li>
+                                @csrf
+                            </ul>
+                        </li>
                     @endif
-
-                    <form method="post" action="{{ route('logout') }}" >
-                        @csrf
-                        <button> logout </button>
-                    </form>
-
-                    <a href="{{ route('profile.show') }}">profile</a>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 @else
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{route('login')}}">Đăng nhập</a>
@@ -49,54 +48,8 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{route('register')}}">Đăng ký</a>
                     </li>
-{{--                    editing--}}
-                    <li class="dropdown py-3">
-                        <a role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="d-flex align-items-center text-body px-3 py-2" style="cursor: pointer;">
-                            <div class="nh-avatar nh-avatar--24 mr-2">
-                                <img data-src="https://static.cdnno.com/user/default/50.jpg" src="https://static.cdnno.com/user/default/50.jpg" lazy="loaded"></div>
-                            griTr84972
-                        </a>
-                        <div aria-labelledby="dropdown-profile" class="dropdown-menu dropdown-menu-right dropdown-menu--profile px-4 rounded-0">
-                            <div class="media mb-4">
-                                <div class="nh-avatar nh-avatar--45 mr-3">
-                                    <img data-src="https://static.cdnno.com/user/default/50.jpg" src="https://static.cdnno.com/user/default/50.jpg" lazy="loaded">
-                                </div>
-                                <div class="media-body">
-                                    <div class="font-weight-semibold mb-1">griTr84972</div>
-                                    <ul class="list-unstyled d-flex flex-wrap">
-                                        <li class="d-flex align-items-center mr-4">
-                                            <i class="svg-icon icon-flower mr-2"></i>
-                                            <a href="javascript:void(0)">0</a>
-                                        </li>
-                                        <li class="d-flex align-items-center">
-                                            <i class="svg-icon icon-candy text-primary mr-2"></i>
-                                            <a href="javascript:void(0)">0</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <ul class="list-unstyled m-0">
-                                <li class="mt-2">
-                                    <a href="/tai-khoan" class="d-block py-2">
-                                        Hồ sơ
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)" class="d-block py-2">
-                                        Đăng xuất
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-
-                    {{--                    end edit--}}
                 @endauth
-
-
             </ul>
-
-
         </div>
     </div>
 </nav>
